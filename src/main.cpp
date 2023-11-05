14d9498f0b59538c396c09d09299dd0eb26801c6
Marek-mba <bereza@gmail.com>
2023-11-05 09:06:16 +0900

    updated to use shared_ptr

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,23 @@
 
-
 #include "App.h"
 
 class MyApp : public App {
-public: 
-	MyApp(Graphics &g) : App(g) {}
+public:
+	MyApp(Graphics &g)
+		: App(g) {}
 
 	void draw() {
 		// g.clear(0, 0, 0);
 		g.setColor(randuf(), randuf(), randuf());
-		g.drawRect(randi(g.width/2), randi(g.height/2), g.width/2, g.height/2);
+		g.drawRect(randi(g.width / 2), randi(g.height / 2), g.width / 2, g.height / 2);
 	}
 };
 
-
-App *instantiateApp(Graphics &g) {
+std::shared_ptr<App> instantiateApp(Graphics &g) {
 	g.width = 512;
 	g.height = 512;
 
-	App *app = new MyApp(g);
+	auto app = std::make_shared<MyApp>(g);
 
 	return app;
 }
-
